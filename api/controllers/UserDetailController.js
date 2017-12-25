d2b65315b330ad093a9808074682f0b2a49800b7
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-12-26 01:07:17 +0700

    create user detail and team api

---
+++ api/controllers/UserDetailController.js
@@ -0,0 +1,11 @@
+/**
+ * UserDetailController
+ *
+ * @description :: Server-side logic for managing userdetails
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Team.js
@@ -0,0 +1,26 @@
+/**
+ * Team.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+     id: {
+        type: 'integer',
+         primaryKey: true,
+         autoIncrement: true
+     },
+     name: {
+        type: 'string'
+     },
+     owner: {
+        model: 'users'
+     },
+     status: {
+        type: 'string'
+     }
+  }
+};
