288b8be9508bc63fbacea2b6c2d25ee0c7231d89
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-12-26 01:33:06 +0700

    create login and register user

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,11 +1,54 @@
 /**
- * UsersController
- *
- * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+* UsersController
+*
+* @description :: Server-side logic for managing users
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
 
 module.exports = {
-	
-};
+	login: function (req, res) {
+		var email = req.param('email');
+		var password = req.param('password');
 
+		if (!email || !password) {
+			return res.json(401, {err: 'email and password required'});
+		}
+
+		Users.findOne({email: email}, function (err, user) {
+			if (!user) {
+				return res.json(401, {err: 'invalid email or password'});
+			}
+
+			Users.comparePassword(password, user, function (err, valid) {
+				if (err) {
+					return res.json(403, {err: 'forbidden'});
+				}
+
+				if (!valid) {
+					//return res.json(401, {err: 'Invalid email or Password'});
+					//request issue no 8
+					return res.json({status:"Invalid email or Password"});
+				} else {
+					//fi issue #7
+					user.avatar = user.avatar || 'holder.jpg';
+					res.json({
+						status:"success",
+						user: user,
+						token: jwToken.issue({id : user.id })
+					});
+				}
+			});
+		})
+	},
+	create: function (req, res) {
+		Users.create(req.body).exec(function (err, users) {
+			if (err) return res.json(err.status, {err: err});
+
+			// If user created successfuly we return user and token as response
+			if (users) {
+				// NOTE: payload is { id: user.id}
+				res.json(200, {user: users, token: jwToken.issue({id: users.id})});
+			}
+		});
+	}
+};
