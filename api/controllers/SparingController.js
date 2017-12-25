f9c6624806c26e418c0b4d66da8fd4cf890ee279
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-12-26 01:28:43 +0700

    create sparing api

---
+++ api/controllers/SparingController.js
@@ -0,0 +1,11 @@
+/**
+ * SparingController
+ *
+ * @description :: Server-side logic for managing sparings
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Sparing.js
@@ -0,0 +1,32 @@
+/**
+ * Sparing.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+     id: {
+         type: 'integer',
+         primaryKey: true,
+         autoIncrement: true
+     },
+     team_a: {
+         model: 'team'
+     },
+     team_b: {
+         model: 'team'
+     },
+     area: {
+         model: 'area'
+     },
+     date_playing: {
+         model: 'string'
+     },
+     status: {
+         type: 'string'
+     }
+  }
+};
